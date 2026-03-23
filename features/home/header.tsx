10b013eaf8976a5ab30795fae5d4ae9259ede139
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2026-03-23 15:13:06 +0530

    AI chat Fixed

--- features/home/header.tsx
+++ features/home/header.tsx
@@ -41,7 +41,7 @@ export function Header() {
                     />
 
                     <span className="hidden sm:block font-extrabold text-lg">
-                      VibeCode Editor
+                      Devflow
                     </span>
                   </Link>
                   <span className="text-zinc-300 dark:text-zinc-700">|</span>
